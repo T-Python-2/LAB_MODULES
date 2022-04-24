39e0b5cf75fd085a54974423c016d10db2af5ea5
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2022-04-24 23:42:24 +0300

    solved

---
+++ dateOP.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+# this function will print the current date 
+def current_time(): 
+    return print (datetime.now())
+
+
+# current_time()
---
+++ main.py
@@ -0,0 +1,5 @@
+# import dateOP
+from dateOP import *
+
+# call the function that print the current date
+current_time()
