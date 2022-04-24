8bc6a04b2b65df47b52ce309cd41a85209e20a77
esrasalam <esra.s.a@outlook.com>
2022-04-24 14:25:09 +0300

    Lab Modules Done

---
+++ dateOP.py
@@ -0,0 +1,8 @@
+
+from datetime import date
+
+def get_today_date():
+
+    today = date.today()
+    print("Today's date : ",today)
+
---
+++ main.py
@@ -0,0 +1,4 @@
+
+from dateOP import get_today_date
+
+get_today_date()
